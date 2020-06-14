cfadc97b11db8d7af253f45054cb6ace6a4b4b99
Alvin Nario <59873164+narioalvin@users.noreply.github.com>
2020-06-14 20:37:41 +0800

    login page

--- src/component/join/join.js
+++ src/component/join/join.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import login from '../../assets/icons/login.svg';
 import './join.css';
 
 const Join = () => {
@@ -8,16 +9,39 @@ const Join = () => {
 
   return (
     <div className="joinOuterContainer">
+      <a className="login-link" target="_blank" href="/react-cheez-meez">
+        Clik here to try it and login with different name but same room.
+      </a>
       <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
+        <img width="150" src={login} />
+
+        <h1 className="heading">CheezMeez</h1>
         <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
         </div>
-        <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+        <div className="mt-20">
+          <select
+            className="room-select"
+            onChange={(event) => setRoom(event.target.value)}
+          >
+            <option default>Select Room</option>
+            <option value="Angular">Angular</option>
+            <option value="Vue">Vue</option>
+            <option value="React">React</option>
+          </select>
         </div>
-        <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-        <button className="button mt-20" type="submit">Sign In</button>
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            JOIN ROOM
+          </button>
         </Link>
       </div>
     </div>
