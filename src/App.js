43940070645ba8c906699c99ece71fe4ce5a13bc
Alvin Nario <59873164+narioalvin@users.noreply.github.com>
2020-06-14 20:38:16 +0800

    added global styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Join from './component/join/join';
 import Chat from './component/chat/chat';
+import './App.css'
 
 const App = () => (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <Route path="/" exact component={Join} />
         <Route path="/chat" exact component={Chat} />
     </Router>
